a1cf92cb1b75041a027019d04de2ffeab1038009
Kayode Segun <kay_1oneway@yahoo.com>
2022-09-09 16:09:22 -0700

    6-print_numberz.c file added

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit < 10)
+	{
+		putchar(digit + 48);
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
