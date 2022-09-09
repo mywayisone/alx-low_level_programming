4650e18bc0da2e0ffa7066b2c7cc73437a0d50d1
Kayode Segun <kay_1oneway@yahoo.com>
2022-09-09 15:43:37 -0700

    5-print_numbers.c file added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit < 10)
+	{
+		printf("℅d", digit);
+		digit++;
+	}
+	printf("\n");
+	return (0);
+}
