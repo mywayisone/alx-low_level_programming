8ab4468b5bcda6b79e55f4e31486ec5b6c7fe3f8
Kayode Segun <kay_1oneway@yahoo.com>
2022-09-09 14:15:55 -0700

    2-print_alphabet.c file added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	return (0);
+}
