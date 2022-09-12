887756be2575ce221a3d6c630a0bd3dccc8b269f
Kayode Segun <kay_1oneway@yahoo.com>
2022-09-11 18:10:43 -0700

    Adding the last file

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main program entry
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	int j;
+	int k;
+	int l;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			k = 0;
+			while (k < 10)
+			{
+				l = 0;
+				while (l < 10)
+				{
+					if (j < l && j != l)
+					{
+						putchar(i + 48);
+						putchar(j + 48);
+						putchar(' ');
+						putcahr(k + 48);
+						putchar(l + 48);
+						if (i + j + l + k != 9 + 8 + 7 + 6)
+						{
+							putchar(',');
+						}
+					}
+					l++;
+				}
+				k++;
+			}
+			j++;
+		}
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
