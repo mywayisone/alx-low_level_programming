a5947aac91d2f79413bf4af51a325e13e8acd59a
Kayode Segun <kay_1oneway@yahoo.com>
2022-09-15 17:26:44 -0700

    function that multiplies two integers.

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,8 +2,9 @@
 
 /**
  * mul - checks for lower case character
- * @a, @b: parameters
- * Return: @a multipied by @b
+ * @a: parameter a
+ * @b: parameter b
+ * Return: a multipied by b
  */
 
 int mul(int a, int b)
