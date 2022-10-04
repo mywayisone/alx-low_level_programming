017cb665345212d136231bfd937b3bf8718610e7
Kayode Segun <kay_1oneway@yahoo.com>
2022-10-03 19:16:37 -0700

    Task 3 corrected

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,53 +2,42 @@
 #include <stdlib.h>
 
 /**
- * str_concat - Concatenates two strings
+ * str_concat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
  *
- * @s1: Pointer to the first string
- *
- * @s2: Pointer to the second string
- *
- * Return: returns pointer to the new string
+ * Return: pointer of an array of chars
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	char *ptr;
-	unsigned int i, j, k;
-	
-	if (s1 != NULL && s2 == NULL)
-	{
-		for (i = 0; s1[i] != '\0'; i++)
-			;
-		ptr = malloc(sizeof(char) * i + 1);
-		for (k = 0; k < i; k++)
-			ptr[k] = str[k];
-		return (ptr);
-	}
-	if (s1 == NULL && s2 != NULL)
-	{
-		for (j = 0; s2[j] != '\0'; j++)
-			;
-		ptr = malloc(sizeof(char) * j + 1);
-		for (k = 0; k < j; k++)
-			ptr[k] = str[k];
-		return (ptr);
-	}
+	char *strout;
+	unsigned int i, j, k, limit;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
 
 	for (j = 0; s2[j] != '\0'; j++)
 		;
-	ptr = malloc(sizeof(char) * (i + j) + 1);
-	if (ptr != NULL)
-	{
-		for (k = 0; k < i + j; k++)
-		{
-			if(k < i)
-				ptr[k] = s1[k];
-			else
-				ptr[k] = s2[k - i];
-		}
-		return  (ptr);
-	}
-	return (ptr);
 
+	strout = malloc(sizeof(char) * (i + j + 1));
+
+	if (strout == NULL)
+	{
+		free(strout);
+		return (NULL);
+	}
+
+	for (k = 0; k < i; k++)
+		strout[k] = s1[k];
+
+	limit = j;
+	for (j = 0; j <= limit; k++, j++)
+		strout[k] = s2[j];
+
+	return (strout);
 }
